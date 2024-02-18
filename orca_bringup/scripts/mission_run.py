b8bc325fe68d7c88cfbfc86dbe55788b13638271
fqbueno <fqbueno@up.edu.ph>
2024-02-18 16:26:37 +0800

    added current progress on line tracing

---
+++ orca_bringup/scripts/mission_run.py
@@ -0,0 +1,425 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+
+import rclpy
+import rclpy.logging
+from action_msgs.msg import GoalStatus
+from geometry_msgs.msg import Point, Pose, PoseStamped
+from nav2_msgs.action import FollowWaypoints
+from orca_msgs.action import TargetMode
+from rclpy.action import ActionClient
+from std_msgs.msg import Header
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Empty
+
+import numpy as np
+import cv2
+import cv_bridge
+
+bridge = cv_bridge.CvBridge()
+
+## User-defined parameters: (Update these values to your liking)
+# Minimum size for a contour to be considered anything
+MIN_AREA = 500 
+
+# Minimum size for a contour to be considered part of the track
+MIN_AREA_TRACK = 5000
+
+# Robot's speed when following the line
+LINEAR_SPEED = 0.2
+
+# Proportional constant to be applied on speed when turning 
+# (Multiplied by the error value)
+KP = 1.5/100 
+
+# If the line is completely lost, the error value shall be compensated by:
+LOSS_FACTOR = 1.2
+
+# Send messages every $TIMER_PERIOD seconds
+TIMER_PERIOD = 0.06
+
+# When about to end the track, move for ~$FINALIZATION_PERIOD more seconds
+FINALIZATION_PERIOD = 4
+
+# The maximum error value for which the robot is still in a straight line
+MAX_ERROR = 30
+
+# BGR values to filter only the selected color range
+lower_bgr_values = np.array([31,  42,  53])
+upper_bgr_values = np.array([255, 255, 255])
+
+def crop_size(height, width):
+    """
+    Get the measures to crop the image
+    Output:
+    (Height_upper_boundary, Height_lower_boundary,
+     Width_left_boundary, Width_right_boundary)
+    """
+    ## Update these values to your liking.
+
+    return (1*height//3, height, width//4, 3*width//4)
+
+image_input = 0
+error = 0
+just_seen_line = False
+just_seen_right_mark = False
+should_move = False
+right_mark_count = 0
+finalization_countdown = None
+
+def start_follower_callback(request, response):
+    """
+    Start the robot.
+    In other words, allow it to move (again)
+    """
+    global should_move
+    global right_mark_count
+    global finalization_countdown
+    should_move = True
+    right_mark_count = 0
+    finalization_countdown = None
+    return response
+
+def stop_follower_callback(request, response):
+    """
+    Stop the robot
+    """
+    global should_move
+    global finalization_countdown
+    should_move = False
+    finalization_countdown = None
+    return response
+
+def image_callback(msg):
+    """
+    Function to be called whenever a new Image message arrives.
+    Update the global variable 'image_input'
+    """
+    global image_input
+    image_input = bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+    print('testing sa image')
+    # node.get_logger().info('Received image')
+
+def get_contour_data(mask, out):
+    """
+    Return the centroid of the largest contour in
+    the binary image 'mask' (the line) 
+    and return the side in which the smaller contour is (the track mark) 
+    (If there are any of these contours),
+    and draw all contours on 'out' image
+    """ 
+    # get a list of contours
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    mark = {}
+    line = {}
+
+    for contour in contours:
+        
+        M = cv2.moments(contour)
+        # Search more about Image Moments on Wikipedia :)
+
+        if M['m00'] > MIN_AREA:
+        # if countor.area > MIN_AREA:
+
+            if (M['m00'] > MIN_AREA_TRACK):
+                # Contour is part of the track
+                line['x'] = crop_w_start + int(M["m10"]/M["m00"])
+                line['y'] = int(M["m01"]/M["m00"])
+
+                # plot the area in light blue
+                cv2.drawContours(out, contour, -1, (255,255,0), 1) 
+                cv2.putText(out, str(M['m00']), (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"])),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255,255,0), 2)
+            
+            else:
+                # Contour is a track mark
+                if (not mark) or (mark['y'] > int(M["m01"]/M["m00"])):
+                    # if there are more than one mark, consider only 
+                    # the one closest to the robot 
+                    mark['y'] = int(M["m01"]/M["m00"])
+                    mark['x'] = crop_w_start + int(M["m10"]/M["m00"])
+
+                    # plot the area in pink
+                    cv2.drawContours(out, contour, -1, (255,0,255), 1) 
+                    cv2.putText(out, str(M['m00']), (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"])),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 2)
+
+
+    if mark and line:
+    # if both contours exist
+        if mark['x'] > line['x']:
+            mark_side = "right"
+        else:
+            mark_side = "left"
+    else:
+        mark_side = None
+
+
+    return (line, mark_side)
+
+def timer_callback():
+    """
+    Function to be called when the timer ticks.
+    According to an image 'image_input', determine the speed of the robot
+    so it can follow the contour
+    """
+
+    global error
+    global image_input
+    global just_seen_line
+    global just_seen_right_mark
+    global should_move
+    global right_mark_count
+    global finalization_countdown
+
+    # Wait for the first image to be received
+    if type(image_input) != np.ndarray:
+        return
+
+    height, width, _ = image_input.shape
+
+    image = image_input.copy()
+
+    global crop_w_start
+    crop_h_start, crop_h_stop, crop_w_start, crop_w_stop = crop_size(height, width)
+
+    # get the bottom part of the image (matrix slicing)
+    crop = image[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop]
+    
+    # get a binary picture, where non-zero values represent the line.
+    # (filter the color values so only the contour is seen)
+    mask = cv2.inRange(crop, lower_bgr_values, upper_bgr_values)
+
+    # get the centroid of the biggest contour in the picture,
+    # and plot its detail on the cropped part of the output image
+    output = image
+    line, mark_side = get_contour_data(mask, output[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop])  
+    # also get the side in which the track mark "is"
+    
+    message = Twist()
+    
+    if line:
+    # if there even is a line in the image:
+    # (as the camera could not be reading any lines)      
+        x = line['x']
+
+        # error:= The difference between the center of the image
+        # and the center of the line
+        error = x - width//2
+
+        message.linear.x = LINEAR_SPEED
+        just_seen_line = True
+
+        # plot the line centroid on the image
+        cv2.circle(output, (line['x'], crop_h_start + line['y']), 5, (0,255,0), 7)
+
+    else:
+        # There is no line in the image. 
+        # Turn on the spot to find it again. 
+        if just_seen_line:
+            just_seen_line = False
+            error = error * LOSS_FACTOR
+        message.linear.x = 0.0
+
+    if mark_side != None:
+        print("mark_side: {}".format(mark_side))
+
+        if (mark_side == "right") and (finalization_countdown == None) and \
+            (abs(error) <= MAX_ERROR) and (not just_seen_right_mark):
+
+            right_mark_count += 1
+
+            if right_mark_count > 1:
+                # Start final countdown to stop the robot
+                finalization_countdown = int(FINALIZATION_PERIOD / TIMER_PERIOD) + 1
+                print("Finalization Process has begun!")
+
+            
+            just_seen_right_mark = True
+    else:
+        just_seen_right_mark = False
+
+    
+    # Determine the speed to turn and get the line in the center of the camera.
+    message.angular.z = float(error) * -KP
+    print("Error: {} | Angular Z: {}, ".format(error, message.angular.z))
+    
+
+
+
+    # Plot the boundaries where the image was cropped
+    cv2.rectangle(output, (crop_w_start, crop_h_start), (crop_w_stop, crop_h_stop), (0,0,255), 2)
+
+    # Uncomment to show the binary picture
+    #cv2.imshow("mask", mask)
+
+    # Show the output image to the user
+    cv2.imshow("output", output)
+    # Print the image for 5milis, then resume execution
+    cv2.waitKey(5)
+
+    # Check for final countdown
+    if finalization_countdown != None:
+        if finalization_countdown > 0:
+            finalization_countdown -= 1
+
+        elif finalization_countdown == 0:
+            should_move = False
+
+
+    # Publish the message to 'cmd_vel'
+    if should_move:
+        publisher.publish(message)
+    else:
+        empty_message = Twist()
+        publisher.publish(empty_message)
+
+class SendGoalResult(Enum):
+    SUCCESS = 0     # Goal succeeded
+    FAILURE = 1     # Goal failed
+    CANCELED = 2    # Goal canceled (KeyboardInterrupt exception)
+
+
+def make_pose(x: float, y: float, z: float):
+    return PoseStamped(header=Header(frame_id='map'), pose=Pose(position=Point(x=x, y=y, z=z)))
+
+
+# Go to AUV mode
+go_auv = TargetMode.Goal()
+go_auv.target_mode = TargetMode.Goal.ORCA_MODE_AUV
+
+# Go to ROV mode
+go_rov = TargetMode.Goal()
+go_rov.target_mode = TargetMode.Goal.ORCA_MODE_ROV
+
+# Go home (1m deep)
+go_home = FollowWaypoints.Goal()
+go_home.poses.append(make_pose(x=0.0, y=0.0, z=-1.0))
+
+# Dive to 8m
+dive = FollowWaypoints.Goal()
+dive.poses.append(make_pose(x=0.0, y=0.0, z=-8.0))
+
+# Big loop, will eventually result in a loop closure
+delay_loop = FollowWaypoints.Goal()
+delay_loop.poses.append(make_pose(x=0.0, y=0.0, z=-2.0))
+# for _ in range(2):
+#     delay_loop.poses.append(make_pose(x=20.0, y=-13.0, z=-7.0))
+#     delay_loop.poses.append(make_pose(x=10.0, y=-23.0, z=-7.0))
+#     delay_loop.poses.append(make_pose(x=-10.0, y=-8.0, z=-7.0))
+#     delay_loop.poses.append(make_pose(x=0.0, y=0.0, z=-7.0))
+
+
+# Send a goal to an action server and wait for the result.
+# Cancel the goal if the user hits ^C (KeyboardInterrupt).
+def send_goal(node, action_client, send_goal_msg) -> SendGoalResult:
+    goal_handle = None
+
+    try:
+        action_client.wait_for_server()
+
+        print('Sending goal...')
+        goal_future = action_client.send_goal_async(send_goal_msg)
+        rclpy.spin_until_future_complete(node, goal_future)
+        goal_handle = goal_future.result()
+
+        if goal_handle is None:
+            raise RuntimeError('Exception while sending goal: {!r}'.format(goal_future.exception()))
+
+        if not goal_handle.accepted:
+            print('Goal rejected')
+            return SendGoalResult.FAILURE
+
+        print('Goal accepted with ID: {}'.format(bytes(goal_handle.goal_id.uuid).hex()))
+        result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(node, result_future)
+
+        result = result_future.result()
+
+        if result is None:
+            raise RuntimeError('Exception while getting result: {!r}'.format(result_future.exception()))
+
+        print('Goal completed')
+        return SendGoalResult.SUCCESS
+
+    except KeyboardInterrupt:
+        # Cancel the goal if it's still active
+        # TODO(clyde): this seems to work, but a second exception is generated -- why?
+        if (goal_handle is not None and
+                (GoalStatus.STATUS_ACCEPTED == goal_handle.status or
+                 GoalStatus.STATUS_EXECUTING == goal_handle.status)):
+            print('Canceling goal...')
+            cancel_future = goal_handle.cancel_goal_async()
+            rclpy.spin_until_future_complete(node, cancel_future)
+            cancel_response = cancel_future.result()
+
+            if cancel_response is None:
+                raise RuntimeError('Exception while canceling goal: {!r}'.format(cancel_future.exception()))
+
+            if len(cancel_response.goals_canceling) == 0:
+                raise RuntimeError('Failed to cancel goal')
+            if len(cancel_response.goals_canceling) > 1:
+                raise RuntimeError('More than one goal canceled')
+            if cancel_response.goals_canceling[0].goal_id != goal_handle.goal_id:
+                raise RuntimeError('Canceled goal with incorrect goal ID')
+
+            print('Goal canceled')
+            return SendGoalResult.CANCELED
+
+
+def main():
+    global node
+    node = None
+    set_target_mode = None
+    follow_waypoints = None
+
+    rclpy.init()
+    global publisher
+
+    try:
+        node = rclpy.create_node("mission_runner")
+
+        set_target_mode = ActionClient(node, TargetMode, '/set_target_mode')
+        follow_waypoints = ActionClient(node, FollowWaypoints, '/follow_waypoints')
+
+        publisher = node.create_publisher(Twist, '/cmd_vel', rclpy.qos.qos_profile_system_default)
+        subscription = node.create_subscription(Image, '/stereo_left',
+                                            image_callback,
+                                            rclpy.qos.qos_profile_sensor_data)
+
+        print('>>> Setting mode to AUV <<<')
+        if send_goal(node, set_target_mode, go_auv) == SendGoalResult.SUCCESS:
+            print('>>> Executing mission <<<')
+            send_goal(node, follow_waypoints, delay_loop)
+            timer = node.create_timer(TIMER_PERIOD, timer_callback)
+
+            start_service = node.create_service(Empty, 'start_follower', start_follower_callback)
+            stop_service = node.create_service(Empty, 'stop_follower', stop_follower_callback)
+            print("testing sa main")
+            rclpy.spin(node)
+
+            # print('>>> Setting mode to ROV <<<')
+            # send_goal(node, set_target_mode, go_rov)
+
+            
+
+            print('>>> Mission complete <<<')
+        else:
+            print('>>> Failed to set mode to AUV, quit <<<')
+
+    finally:
+        if set_target_mode is not None:
+            set_target_mode.destroy()
+        if follow_waypoints is not None:
+            follow_waypoints.destroy()
+        if node is not None:
+            node.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
